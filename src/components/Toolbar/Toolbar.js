90a62ca21e56b3407fdf5f9c3b012043ec772dba
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-03-17 15:16:24 +0600

    Created Toolbar component

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,10 @@
+import classes from "./Toolbar.module.css"
+
+const Layout = ({ }) => {
+    return ( 
+        <div className={classes.Toolbar}>
+        </div>
+     );
+}
+ 
+export default Layout;
