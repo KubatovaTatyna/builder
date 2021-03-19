b4643b9e402769665c9d8708157ecf2abf4ce04f
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-03-19 15:36:23 +0600

    Add Drawer open and close functions

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,18 +1,23 @@
-import classes from "./Drawer.module.css"
 import Logo from "../Logo/Logo";
-import Nav from "../Nav/Nav"
-import Backdrop from "../UI/Backdrop/Backdrop"
+import Nav from "../Nav/Nav";
+import Backdrop from "../UI/Backdrop/Backdrop";
+import classes from "./Drawer.module.css";
 
-const Drawer = () => {
-    return ( 
-        <div className={classes.Drawer}>
-            <Backdrop />
-            <div className={classes.content}>
-            <Logo />
-            <Nav />
-            </div>
-        </div>
-     );
+const Drawer = ({ open, closeDrawer }) => {
+  const drawerClasses = [
+    classes.content,
+    open ? classes.open : classes.close
+  ];
+
+  return (
+    <div className={classes.Drawer}>
+      <Backdrop show={open} click={closeDrawer} />
+      <div className={drawerClasses.join(" ")}>
+        <Logo />
+        <Nav />
+      </div>
+    </div>
+  );
 }
  
 export default Drawer;
