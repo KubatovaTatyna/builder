67e62ffd54e7a52184b4024cd5825e01bb1d2124
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-03-27 20:06:07 +0600

    Added new props and used them

--- src/components/HouseplantShop/HouseplantControls/HouseplantControls.js
+++ src/components/HouseplantShop/HouseplantControls/HouseplantControls.js
@@ -1,9 +1,10 @@
+import HouseplantControl from "./HouseplantControl";
 import classes from "./HouseplantControls.module.css";
 
-const HouseplantControls = ({pots}) => {
+const HouseplantControls = ({pots , addPot , removePot}) => {
     const results =[];
     for (const pot in pots) {
-        results.push(pot)
+        results.push(<HouseplantControl type={pot} addPot={addPot} removePot={removePot}/>)
     }
     return ( 
         <div className={classes.HouseplantControls}>{results}</div>
