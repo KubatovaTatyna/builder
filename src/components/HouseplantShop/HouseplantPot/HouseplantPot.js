0832bb1b24ec6814cc17ae5f8f4da75875931abc
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-04-03 00:37:35 +0600

    Deleted pots object with css style

--- src/components/HouseplantShop/HouseplantPot/HouseplantPot.js
+++ src/components/HouseplantShop/HouseplantPot/HouseplantPot.js
@@ -1,22 +1,9 @@
 import classes from "./HouseplantPot.module.css"
 
-import alocasia from "../../../images/alocasia.svg"
-import begonia from "../../../images/begonia.svg"
-import bonsai from "../../../images/bonsai.svg"
-import calathea from "../../../images/calathea.svg"
-import cyclamen from "../../../images/cyclamen.svg"
-import dracaena from "../../../images/dracaena.svg"
 const HouseplantPot =({type}) => {
-    const types = {
-        alocasia : {backgroundImage : `url(${alocasia})` , width:"80px", height:"80px"},
-        begonia : {backgroundImage : `url(${begonia})` , width:"80px", height:"80px"},
-        bonsai : {backgroundImage : `url(${bonsai})` , width:"80px", height:"80px"},
-        calathea : {backgroundImage : `url(${calathea})` , width:"80px", height:"80px"},
-        cyclamen : {backgroundImage : `url(${cyclamen})` , width:"80px", height:"80px"},
-        dracaena : {backgroundImage : `url(${dracaena})` , width:"80px", height:"80px"}
-    }
+    const pots = [classes.HouseplantPot , classes[type]]
     return ( 
-        <div className={classes.HouseplantPot} style={types[type]}></div>
+        <div className={pots.join(" ")}></div>
      );
 }
  
