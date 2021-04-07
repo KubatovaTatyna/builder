aadf33d5f53925b4d491c867420929b4c94a2729
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-04-07 14:46:00 +0600

    Add order button

--- src/components/HouseplantShop/HouseplantControls/HouseplantControls.js
+++ src/components/HouseplantShop/HouseplantControls/HouseplantControls.js
@@ -3,11 +3,16 @@ import classes from "./HouseplantControls.module.css";
 
 const HouseplantControls = ({pots , addPot , removePot}) => {
     const results =[];
+    let total = 0;
     for (const pot in pots) {
+        total += pots[pots]
         results.push(<HouseplantControl type={pot} addPot={addPot} removePot={removePot}/>)
     }
     return ( 
-        <div className={classes.HouseplantControls}>{results}</div>
+        <div className={classes.HouseplantControls}>
+            {results}
+            <button disabled={!total}>Order</button>
+        </div>
      );
 }
  
