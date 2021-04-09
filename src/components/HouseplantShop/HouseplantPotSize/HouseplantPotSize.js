d9d2a25c49c08942a2e85a6c62e759ebedd563ca
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-04-09 22:23:20 +0600

    Change some styles

--- src/components/HouseplantShop/HouseplantPotSize/HouseplantPotSize.js
+++ src/components/HouseplantShop/HouseplantPotSize/HouseplantPotSize.js
@@ -7,12 +7,12 @@ import cyclamen from "../../../images/cyclamen.svg"
 import dracaena from "../../../images/dracaena.svg"
 const HouseplantPotSize =({type}) => {
     const types = {
-        alocasia : {backgroundImage : `url(${alocasia})` , width:"50px", height:"50px"},
-        begonia : {backgroundImage : `url(${begonia})` , width:"50px", height:"50px"},
-        bonsai : {backgroundImage : `url(${bonsai})` , width:"50px", height:"50px"},
-        calathea : {backgroundImage : `url(${calathea})` , width:"50px", height:"50px"},
-        cyclamen : {backgroundImage : `url(${cyclamen})` , width:"50px", height:"50px"},
-        dracaena : {backgroundImage : `url(${dracaena})` , width:"50px", height:"50px"}
+        alocasia : {backgroundImage : `url(${alocasia})` , width:"60px", height:"60px"},
+        begonia : {backgroundImage : `url(${begonia})` , width:"60px", height:"60px"},
+        bonsai : {backgroundImage : `url(${bonsai})` , width:"60px", height:"60px"},
+        calathea : {backgroundImage : `url(${calathea})` , width:"60px", height:"60px"},
+        cyclamen : {backgroundImage : `url(${cyclamen})` , width:"60px", height:"60px"},
+        dracaena : {backgroundImage : `url(${dracaena})` , width:"60px", height:"60px"}
     }
     return ( 
         <div style={types[type]}></div>
