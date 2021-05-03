b123f307b575da38664d6e3f47c65830ffaba1a6
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-05-03 15:11:18 +0600

    Add and style Checkout Form component

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,13 +1,44 @@
-import CheckoutSummary from "./CheckoutSummary/CheckoutSummary";
+
+import classes from "./Checkout.module.css";
+import HouseplantPreview from "../HouseplantShop/HouseplantPreview/HouseplantPreview";
+import CheckoutForm from "./CheckoutForm/CheckoutForm";
+import axios from "axios";
 
 const Checkout = ({ history }) => {
     function cancelCallback() {
       history.replace('/');
     }
+    function submitCallback(event) {
+      const data = new FormData(event.target);
+  
+      axios.post('https://builder-b9129-default-rtdb.firebaseio.com/orders.json', {
+        name: data.get('name'),
+        address: data.get('address'),
+        phone: data.get('phone'),
+        pots: {
+          alocasia: 10,
+          bonsai: 10,
+          begonia: 10,
+          dracaena: 10,
+          cyclmen: 10,
+          calathea: 10,
+        },
+        price: 100,
+      }).then(response => {
+        history.replace('/');
+      });
+  
+      event.preventDefault();
+    }
     return (
-        <div>
-          <CheckoutSummary cancelCallback={cancelCallback} />
-        </div>
+      <div className={classes.Checkout}>
+      <HouseplantPreview pots={{
+        alocasia: 5, 
+        bonsai: 1,
+        begonia: 5,
+      }} price={150} />
+      <CheckoutForm cancelCallback={cancelCallback} submitCallback={submitCallback}/>
+    </div>
       );
     }
 
