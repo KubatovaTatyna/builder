c6e05c8fdc9b1477b58cad7d0b64128a9cd43538
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-05-06 12:36:47 +0600

    changed modal component color

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,45 +1,42 @@
-
-import classes from "./Checkout.module.css";
 import HouseplantPreview from "../HouseplantShop/HouseplantPreview/HouseplantPreview";
 import CheckoutForm from "./CheckoutForm/CheckoutForm";
+import classes from "./Checkout.module.css";
 import axios from "axios";
+import { useSelector } from "react-redux";
+
 
 const Checkout = ({ history }) => {
-    function cancelCallback() {
-      history.replace('/');
-    }
-    function submitCallback(event) {
-      const data = new FormData(event.target);
-  
-      axios.post('https://builder-b9129-default-rtdb.firebaseio.com/orders.json', {
-        name: data.get('name'),
-        address: data.get('address'),
-        phone: data.get('phone'),
-        pots: {
-          alocasia: 10,
-          bonsai: 10,
-          begonia: 10,
-          dracaena: 10,
-          cyclmen: 10,
-          calathea: 10,
-        },
-        price: 100,
-      }).then(response => {
-        history.replace('/');
-      });
-  
-      event.preventDefault();
-    }
-    return (
-      <div className={classes.Checkout}>
-      <HouseplantPreview pots={{
-        alocasia: 5, 
-        bonsai: 1,
-        begonia: 5,
-      }} price={150} />
-      <CheckoutForm cancelCallback={cancelCallback} submitCallback={submitCallback}/>
-    </div>
-      );
-    }
+  const pots = useSelector(state => state.pots);
+  const price = useSelector(state => state.price);
 
+  function cancelCallback() {
+    history.replace('/');
+  }
+
+  function submitCallback(event) {
+    const data = new FormData(event.target);
+
+    axios.post('https://builder-b9129-default-rtdb.firebaseio.com/orders.json', {
+      name: data.get('name'),
+      address: data.get('address'),
+      phone: data.get('phone'),
+      pots: pots,
+      price: price,
+    }).then(response => {
+      history.replace('/');
+    });
+
+    event.preventDefault();
+  }
+
+  return (
+    <div className={classes.Checkout}>
+      <HouseplantPreview pots={pots} price={price} />
+      <CheckoutForm
+        cancelCallback={cancelCallback}
+        submitCallback={submitCallback} />
+    </div>
+  );
+}
+ 
 export default Checkout;
