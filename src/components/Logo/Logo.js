3363e13bffed570e840a428d691620b97c0e72d5
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-05-10 11:12:48 +0600

    Change logo of project

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,9 @@
 import classes from "./Logo.module.css"
-import logo from "../../images/logo.svg"
+import logo2 from "../../images/logo2.svg"
 const Logo = () => {
     return ( 
         <div className={classes.Logo}>
-            <img  alt="Logo of the Houseplant project" src={logo} className={classes.Logo} />
+            <img  alt="Logo of the Houseplant project" src={logo2} className={classes.Logo} />
             <div>Houseplant</div>
         </div>
      );
